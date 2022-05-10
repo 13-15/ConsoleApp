8f8a1e608fa94afee69103c944b4a3dd674fcc89
SkyNet <SkyNet@DESKTOP-S5AVU9L>
2022-05-10 13:48:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_OOP
+{
+    class Character
+    {
+        public int Health = 100;
+        public void Hit(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace D_OOP
+{
+    class Programm
+    {
+        static void Main(string[] arg)
+        {
+            Character d = new Character();
+            d.Hit(56);
+            Console.WriteLine("здоровье " + d.Health );
+        }
+    }
+}
